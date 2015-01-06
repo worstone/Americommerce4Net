5d44306839cbbef0ef06c6c3079e068258902831
Ken Worst <ken.worst@gmail.com>
2015-01-06 14:40:45 -0800

    Unit test changes

---
+++ Americommerce4Net_Tests/ClientPeople_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Americommerce4Net;
+
+namespace Americommerce4Net_Tests
+{
+    [TestFixture]
+    public class ClientPeople_Tests
+    {
+        [Test]
+        public void Test() {
+            var client = new ClientPeople();
+            var response = client.Customers.Get(1);
+            
+        }
+    }
+}
