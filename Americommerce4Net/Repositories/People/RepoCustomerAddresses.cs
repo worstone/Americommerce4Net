b715b9a8f8a2aaf21e771494005b804b308aeb09
Ken Worst <ken.worst@gmail.com>
2015-03-04 17:23:28 -0800

    Fixed RepoCustomerAddresses  had wrong type

--- Americommerce4Net/Repositories/People/RepoCustomerAddresses.cs
+++ Americommerce4Net/Repositories/People/RepoCustomerAddresses.cs
@@ -2,7 +2,7 @@ using Americommerce4Net.Models;
 
 namespace Americommerce4Net.Repositories
 {
-    public class RepoCustomerAddresses : BaseRepoReadWrite<CustomerType>
+    public class RepoCustomerAddresses : BaseRepoReadWrite<Customer_Address>
     {
         readonly static IClientReadWrite _Client = new ClientPeople().Addresses;
 
